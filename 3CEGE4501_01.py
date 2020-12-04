39af982cff10ebe8e922dcab42c5ef2caf3d9742
Hoff1363 <75404460+Hoff1363@users.noreply.github.com>
2020-12-04 10:11:07 -0600

    Add files via upload

---
+++ 3CEGE4501_01.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # <font color='maroon'> Water and Human Societies</font>
+
+# Spread of human populations and formation of civilizations are correlated with the availability of fresh water. The **Mesopotamian civilization** widely considered as the cradle of civilization, was in the the area between the **Tigris and Euphrates Rivers** Contemporary distribution of human population in the United State is not an exemption. It is easy to see that the climatology of precipitation somewhat has dictated the density of human population in the US. Most of the biggest cities and economies of the country are in the vicinity of lakes, rivers and estuaries.
+
+# The explained correlation clearly does not come as a surprise because water is an essential element not only for survival of human populations but it is the most critical ingredient for food production, formation of central governance, and thus fundamental to human societies.
+
+# <font color='red'>**Hydrology**</font>  is the study of water and its transport in all of its phases (i.e., solid, liquid, vapor) across different elements of **hydrosphere**, which is a region between 1 km deep in the lithosphere and 15 km high in the atmosphere. These days, *hydrology is a science* and largely focuses on **terrestrial water cycle** in continental and global scales under **natural condition**, without direct human control or intervention, which makes it distinct from classic *hydrologic and hydraulic engineering*.
+
+# ![US_population.png](attachment:US_population.png)
+
+# ![PRISM_30_annual_precip.png](attachment:PRISM_30_annual_precip.png)
+
+# In[ ]:
+
+
+display(HTML("<table><tr><td><img src='PRISM_30_annual_precip.png'></td><td><img src='US_population.png'></td></tr></table>"))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
